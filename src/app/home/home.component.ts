a3975c281b3c609ae2b157a098dbba95d97f3f5b
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-02 16:48:27 +0000

    observer.error and observer.complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Observable, Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,16 +15,27 @@ export class HomeComponent implements OnInit, OnDestroy {
     /* this.firstObsSubscription = interval(1000).subscribe(count => {
       console.log(count);
     }) */
-    const customInternalObservable = Observable.create(observer => {
+    const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if(count === 2) {
+          observer.complete();
+        }
+        if(count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customInternalObservable.subscribe(data => {
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     })
   }
 
